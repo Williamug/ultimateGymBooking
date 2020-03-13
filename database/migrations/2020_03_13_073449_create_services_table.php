cc77b2749727a2eadda54453cff88b2e74a6ce09
williamdk <asabawilliam@gmail.com>
2020-03-13 10:53:42 +0300

    add service mode and services migration

---
+++ database/migrations/2020_03_13_073449_create_services_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('services', function (Blueprint $table) {
+				$table->id();
+				$table->string('title');
+				$table->string('service_duration');
+				$table->integer('available_seats');
+				$table->text('description');
+				$table->time('service_starts_at');
+				$table->time('service_ends_at');
+				$table->integer('allow_booking_max_days_ago');
+				$table->string('service_duration_type');
+				$table->boolean('status');
+				$table->string('days');
+				$table->UnsignedInteger('payments_id');
+				$table->timestamps();
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('services');
+	}
+}
