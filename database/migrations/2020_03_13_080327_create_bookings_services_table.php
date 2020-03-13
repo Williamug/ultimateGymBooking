e6f230288c82df24f674f7b2f677342428cba242
williamdk <asabawilliam@gmail.com>
2020-03-13 11:07:14 +0300

    add booking_service relation table and the migration

---
+++ database/migrations/2020_03_13_080327_create_bookings_services_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsServicesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('bookings_services', function (Blueprint $table) {
+				$table->id();
+				$table->unsignedInteger('bookings_id');
+				$table->unsignedInteger('services_id');
+				$table->timestamps();
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('bookings_services');
+	}
+}
