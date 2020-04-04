bf56b88cdb90823d792f48508c9a939bb7a35b52
williamdk <asabawilliam@gmail.com>
2020-04-04 14:20:09 +0300

    add currencies table

---
+++ database/migrations/2020_04_02_145151_create_currencies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('currencies', function (Blueprint $table) {
+				$table->id();
+				$table->unsignedInteger('settings_id');
+				$table->string('currency', 45);
+				$table->string('currency_symbol', 4);
+				$table->string('currency_long_name');
+				$table->timestamps();
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('currencies');
+	}
+}
