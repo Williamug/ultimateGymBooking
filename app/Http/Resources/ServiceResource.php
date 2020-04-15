e983289695438e1a19f659521c3adaa8f8e1ea23
williamdk <asabawilliam@gmail.com>
2020-04-15 23:18:02 +0300

    add service resource file

---
+++ app/Http/Resources/ServiceResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\ServiceResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ServiceResource extends JsonResource {
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function toArray($request) {
+		// return parent::toArray($request);
+		return [
+			'id'                         => $this->id,
+			'title'                      => $this->title,
+			'service_duration'           => $this->service_duration,
+			'available_seats'            => $this->available_seats,
+			'description'                => $this->description,
+			'service_starts_at'          => $this->service_starts_at,
+			'service_ends_at'            => $this->service_ends_at,
+			'allow_booking_max_days_ago' => $this->allow_booking_max_days_ago,
+			'service_duration_type'      => $this->service_duration_type,
+			'status'                     => $this->status,
+			'days'                       => $this->days,
+			'currencies'                 => new CurrencyResource($this->currencies),
+		];
+	}
+
+	public function with($request) {
+		return [
+			'version'       => '1.0.0',
+			'author'        => 'Asaba William (williamDk)',
+			'author_url'    => 'http://williamug.wordpress.com',
+			'author_github' => 'http://github.com/williamug',
+		];
+	}
+}
