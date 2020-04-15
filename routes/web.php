784f4dddb38a533db95eb9595c08a8d1f668c725
williamdk <asabawilliam@gmail.com>
2020-04-15 15:03:18 +0300

    add settings routes

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,8 @@ function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// settings
 Route::get('settings', 'API\SettingsController@index');
+Route::post('settings', 'API\SettingsController@store');
+Route::put('settings/{setting}', 'API\SettingsController@update');
