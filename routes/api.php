2e367ae974daa457e9de329192ba405dd82b6b87
williamdk <asabawilliam@gmail.com>
2020-04-15 15:02:58 +0300

    add settings and currencies routes

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
- */
+// Route::middleware('auth:api')->get('/user',
 
-Route::middleware('auth:api')->get('/user',
+// function (Request $request) {
+// 		// return $request->user();
+// 	});
 
-function (Request $request) {
-		// return $request->user();
-	});
+// settings
 Route::get('settings', 'API\SettingsController@index');
+Route::post('settings', 'API\SettingsController@store');
+Route::put('settings/{setting}', 'API\SettingsController@update');
+Route::delete('settings/{setting}', 'API\SettingsController@destroy');
+
+// currencies
 Route::get('currencies', 'API\CurrenciesController@index');
+Route::post('currencies', 'API\CurrenciesController@store');
+Route::get('currencies/{currency}', 'API\CurrenciesController@show');
+Route::patch('currencies/{currency}', 'API\CurrenciesController@update');
+Route::delete('currencies/{currency}', 'API\CurrenciesController@destroy');
