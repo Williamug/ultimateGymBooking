b8671f38902ec5fd1a8d56f318d3ca4da13c07d7
williamdk <asabawilliam@gmail.com>
2020-04-15 15:02:23 +0300

    remove fax number, logo and timezone

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -11,15 +11,13 @@ class SettingsTableSeeder extends Seeder {
 	 */
 	public function run() {
 		DB::table('settings')->insert([
+				'currency_id'             => 2,
 				'company_name'            => 'Ultimate Gym Booking',
 				'address'                 => 'Plot 3 Industrial area, Design Hub Kampala',
 				'phone_number'            => '122344566',
-				'fax_number'              => '122347877',
 				'official_company_number' => '0122347877',
 				'email'                   => 'ultimategymbooking@gmail.com',
 				'logo'                    => 'logo.jpg',
-				'logo'                    => 'logo.jpg',
-				'timezone'                => 'Africa/Nairobi',
 				'created_at'              => now(),
 				'updated_at'              => now(),
 			]);
