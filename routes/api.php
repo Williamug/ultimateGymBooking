0532ed5dac5887043fc95d7ec20427c824eddce1
williamdk <asabawilliam@gmail.com>
2020-04-20 10:59:23 +0300

    Add instructors, clients and admin routes

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::post('roles', 'API\RolesController@store');
 Route::get('roles/{role}', 'API\RolesController@show');
 Route::patch('roles/{role}', 'API\RolesController@update');
 Route::delete('roles/{role}', 'API\RolesController@destroy');
+// Route::apiResource('roles', 'API\RolesController');
 
 // service
 Route::get('services', 'API\ServicesController@index');
@@ -34,3 +35,24 @@ Route::post('services', 'API\ServicesController@store');
 Route::get('services/{service}', 'API\ServicesController@show');
 Route::patch('services/{service}', 'API\ServicesController@update');
 Route::delete('services/{service}', 'API\ServicesController@destroy');
+
+// instructor
+Route::get('instructors', 'API\InstructorsController@index');
+Route::post('instructors', 'API\InstructorsController@store');
+Route::get('instructors/{instructor}', 'API\InstructorsController@show');
+Route::patch('instructors/{instructor}', 'API\InstructorsController@update');
+Route::delete('instructors/{instructor}', 'API\InstructorsController@destroy');
+
+// clients
+Route::get('clients', 'API\ClientsController@index');
+Route::post('clients', 'API\ClientsController@store');
+Route::get('clients/{client}', 'API\ClientsController@show');
+Route::patch('clients/{client}', 'API\ClientsController@update');
+Route::delete('clients/{client}', 'API\ClientsController@destroy');
+
+// admins
+Route::get('admins', 'API\AdminsController@index');
+Route::post('admins', 'API\AdminsController@store');
+Route::get('admins/{admin}', 'API\AdminsController@show');
+Route::patch('admins/{admin}', 'API\AdminsController@update');
+Route::delete('admins/{admin}', 'API\AdminsController@destroy');
