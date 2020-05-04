af1b1941e890172cc9d56d02ef4d157444c77091
williamdk <asabawilliam@gmail.com>
2020-05-04 11:03:38 +0300

    update settings controller

--- app/Http/Controllers/web/SettingsController.php
+++ app/Http/Controllers/web/SettingsController.php
@@ -4,20 +4,28 @@ namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
 use App\Model\Currency;
+use App\Model\EmailSetting;
+use App\Model\EmailTemplate;
+use App\Model\Role;
 use App\Model\Setting;
 use Illuminate\Http\Request;
 
-class SettingsController extends Controller {
+class SettingsController extends Controller
+{
 	/**
 	 * Display a listing of the resource.
 	 *
 	 * @return \Illuminate\Http\Response
 	 */
-	public function index() {
-		$setting    = Setting::first();
-		$currencies = Currency::all();
+	public function index()
+	{
+		$setting        = Setting::first();
+		$currencies     = Currency::all();
+		$emailSetting   = EmailSetting::first();
+		$emailtemplates = EmailTemplate::all();
+		$roles = Role::all();
 
-		return view('settings.index', compact('setting', 'currencies'));
+		return view('settings.index', compact('setting', 'currencies', 'emailSetting', 'emailtemplates', 'roles'));
 	}
 
 	/**
@@ -27,20 +35,20 @@ class SettingsController extends Controller {
 	 * @param  \App\Model\Setting  $setting
 	 * @return \Illuminate\Http\Response
 	 */
-	public function update(Request $request, Setting $setting) {
+	public function update(Request $request, Setting $setting)
+	{
 
 		$data = request()->validate([
-				'currency_id'             => '',
-				'company_name'            => 'required',
-				'address'                 => 'required',
-				'phone_number'            => '',
-				'official_company_number' => 'required',
-				'email'                   => '',
-				'website'                 => '',
-				'logo'                    => '',
-			]);
+			'currency_id'             => 'numeric',
+			'company_name'            => 'required',
+			'address'                 => 'required',
+			'phone_number'            => '',
+			'official_company_number' => 'required',
+			'email'                   => '',
+			'website'                 => '',
+			'logo'                    => '',
+		]);
 		$setting->update($data);
-		return redirect('settings')->with('message', 'Your information has been updated');
+		return redirect('settings')->with('success-message', 'Your information has been updated');
 	}
-
 }
