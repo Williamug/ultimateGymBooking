5820d89070a394cc521229648873748996ee319c
williamdk <asabawilliam@gmail.com>
2020-05-07 00:41:24 +0300

    services controller

--- app/Http/Controllers/web/ServicesController.php
+++ app/Http/Controllers/web/ServicesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
+use App\Model\Instructor;
 use App\Model\Service;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class ServicesController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function index() {
-		$services = Service::all();
+		$services = Service::where('id', '>', 0)->orderBy('id', 'desc')->paginate(10);
 		return view('services.index', compact('services'));
 	}
 
@@ -23,7 +24,9 @@ class ServicesController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function create() {
-		return view('services.create');
+		$instructors = Instructor::all();
+
+		return view('services.create', compact('instructors'));
 	}
 
 	/**
@@ -33,7 +36,26 @@ class ServicesController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function store(Request $request) {
-		//
+
+		$instructor = Instructor::where('id', $request['instructor_id'])->get();
+
+		$data = request()->validate([
+				'price'                      => 'required',
+				'title'                      => 'required|unique:App\Model\Service,title',
+				'service_duration'           => '',
+				'available_seats'            => 'required',
+				'description'                => '',
+				'service_starts_at'          => 'required',
+				'service_ends_at'            => 'required',
+				'allow_booking_max_days_ago' => '',
+				'service_duration_type'      => '',
+				'status'                     => '',
+				'days'                       => '',
+			]);
+		$services = Service::create($data);
+
+		$services->instructors()->attach($instructor);
+		return redirect('services/create')->with('message', 'A new service has been added successfully');
 	}
 
 	/**
@@ -43,6 +65,7 @@ class ServicesController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function show(Service $service) {
+
 		return view('services.show', compact('service'));
 	}
 
@@ -53,7 +76,8 @@ class ServicesController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function edit(Service $service) {
-		//
+		$instructors = Instructor::all();
+		return view('services.edit', compact('service', 'instructors'));
 	}
 
 	/**
@@ -64,7 +88,24 @@ class ServicesController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function update(Request $request, Service $service) {
-		//
+		$instructor = Instructor::where('id', $request['instructor_id'])->get();
+		$data       = request()->validate([
+				'price'                      => 'required',
+				'title'                      => 'required',
+				'service_duration'           => '',
+				'available_seats'            => 'required',
+				'description'                => '',
+				'service_starts_at'          => 'required',
+				'service_ends_at'            => 'required',
+				'allow_booking_max_days_ago' => '',
+				'service_duration_type'      => '',
+				'status'                     => '',
+				'days'                       => '',
+			]);
+		$service->update($data);
+		$service->instructors()->detach();
+		$service->instructors()->attach($instructor);
+		return redirect()->route('services.show', ['service' => $service])->with('message', 'Service has been updated');
 	}
 
 	/**
@@ -74,6 +115,27 @@ class ServicesController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function destroy(Service $service) {
-		//
+		$service->delete();
+		$service->instructors()->detach();
+		return redirect('services')->with('message', 'This service has been deleted permanently');
+	}
+
+	// validation
+	public function serviceValidationRules() {
+		request()->validate([
+				'price'                      => 'required',
+				'title'                      => 'required',
+				'service_duration'           => '',
+				'available_seats'            => 'required',
+				'instructor_id'              => 'required',
+				'description'                => '',
+				'service_starts_at'          => 'required',
+				'service_ends_at'            => 'required',
+				'allow_booking_max_days_ago' => '',
+				'service_duration_type'      => '',
+				'status'                     => '',
+				'days'                       => '',
+			]);
+
 	}
 }
