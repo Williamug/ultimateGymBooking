377a42efd443c54d4ea779195adedec8fc428dc3
williamdk <asabawilliam@gmail.com>
2020-05-10 15:10:13 +0300

    add bookings controller and bookings/index.blade.php

---
+++ resources/views/bookings/index.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.master')
+
+@section('title', 'Clients')
+
+@section('content')
+@if(session()->has('message'))
+    <div class="alert alert-danger">{{ session()->get('message') }}</div>
+@endif
+<div class="card">
+	<div class="card-header">
+		<h4 class="card-title">Bookings</h4>
+
+		<div class="card-tools">
+			<div class="input-group input-group-sm" style="width: 150px;">
+				<a href="{{ route('clients.create') }}" class="btn bg-gradient-primary btn-sm">Add New Booking <i
+						class="fas fa-plus"></i></a>
+			</div>
+		</div>
+	</div>
+	<!-- /.card-header -->
+	<div class="card-body table-responsive p-0">
+		<table class="table table-hover text-nowrap">
+			<thead>
+				<tr>
+					<th>Service</th>
+					<th>Client</th>
+					<th>Status</th>
+					<th>Date</th>
+					<th>Bill</th>
+					<th>Payment Status</th>
+					<th>Phone Number</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			@foreach($bookings as $booking)
+			<tbody>
+				<tr>
+					<td>{{ $booking }}</td>
+					<td>{{ $booking }}</td>
+					<td>{{ $booking }}</td>
+					<td>{{ $booking }}</td>
+					<td>
+						{{-- @if($client->user->email_verified_at !== null)
+						<small class="badge badge-primary">Verified</small>
+						@else
+						<small class="badge badge-warning">Pending</small>
+						@endif --}}
+					</td>
+					<td>{{ $booking }}</td>
+
+					<td>
+                        <div class="row">
+                            <div class="col-md-6">
+						          <a href="{{ route('clients.show', ['client' => $booking]) }}" title="View" class="btn bg-gradient-primary btn-sm">
+							         <i class="fas fa-eye"></i>
+						          </a>
+                            </div>
+                            <div class="">
+                                <form action="{{ route('clients.destroy', ['client' => $booking]) }}" method="post">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn bg-gradient-danger btn-sm" title="Delete">
+                                        <i class="far fa-trash-alt"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+					</td>
+				</tr>
+			</tbody>
+			@endforeach
+		</table>
+
+		{{-- <div class="ml-4 mt-4">{{ $clients->links() }}
+	</div> --}}
+</div>
+<!-- /.card-body -->
+</div>
+
+@include('models.services')
+@endsection
