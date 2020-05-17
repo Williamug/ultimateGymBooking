00d00c983b7c291a871c0790e8000d1ba4ece449
williamdk <asabawilliam@gmail.com>
2020-05-17 13:14:28 +0300

    remove sweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,17 +6,7 @@
 
 require('./bootstrap');
 import { Form, HasError, AlertError } from 'vform';
-import Swal from 'sweetalert2';
 
-window.Swal = Swal;
-const Toast = Swal.mixin({
-	toast: true,
-	position: 'top-end',
-	showConfirmButton: false,
-	timer: 3000
-	});
-
-window.Toast = Toast;
 
 window.Vue = require('vue');
 window.Form = Form;
