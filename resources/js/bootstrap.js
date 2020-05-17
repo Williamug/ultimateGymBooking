99f00fe38cc4248caddef9905c9d5d086a3e0ad1
williamdk <asabawilliam@gmail.com>
2020-05-17 13:15:14 +0300

    require chartjs and moment

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,9 +9,11 @@ window._ = require('lodash');
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
+    window.moment = require('moment'); 
 
     require('bootstrap');
     require('admin-lte');
+    require('chart.js');
 } catch (e) {}
 
 /**
