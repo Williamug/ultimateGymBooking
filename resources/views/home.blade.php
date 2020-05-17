2222b67e0632961eaac3323de02f7ba1b7659216
williamdk <asabawilliam@gmail.com>
2020-05-17 13:17:36 +0300

    Add render chart to the home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,8 +96,8 @@
               <div class="card-body">
                 <div class="d-flex">
                   <p class="d-flex flex-column">
-                    <span class="text-bold text-lg">820</span>
-                    <span>Visitors Over Time</span>
+                    <span class="text-bold text-lg">{{ $bookings->count() }}</span>
+                    <span>Bookings Over Time</span>
                   </p>
                   <p class="ml-auto d-flex flex-column text-right">
                     <span class="text-success">
@@ -109,17 +109,9 @@
                 <!-- /.d-flex -->
 
                 <div class="position-relative mb-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
-                  <canvas id="visitors-chart" height="200" width="445" class="chartjs-render-monitor" style="display: block; width: 445px; height: 200px;"></canvas>
-                </div>
-
-                <div class="d-flex flex-row justify-content-end">
-                  <span class="mr-2">
-                    <i class="fas fa-square text-primary"></i> This Week
-                  </span>
-
-                  <span>
-                    <i class="fas fa-square text-gray"></i> Last Week
-                  </span>
+                  {{-- <canvas id="visitors-chart" height="200" width="445" class="chartjs-render-monitor" style="display: block; width: 445px; height: 200px;"></canvas> --}}
+                      {{-- Chart goes here --}}
+                  {!! $chart->container() !!}
                 </div>
               </div>
             </div>
@@ -150,7 +142,7 @@
                 <div class="d-flex">
                   <p class="d-flex flex-column">
                     <span class="text-bold text-lg">820</span>
-                    <span>Visitors Over Time</span>
+                    <span>Sales Over Time</span>
                   </p>
                   <p class="ml-auto d-flex flex-column text-right">
                     <span class="text-success">
@@ -162,17 +154,7 @@
                 <!-- /.d-flex -->
 
                 <div class="position-relative mb-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
-                  <canvas id="visitors-chart" height="200" width="445" class="chartjs-render-monitor" style="display: block; width: 445px; height: 200px;"></canvas>
-                </div>
-
-                <div class="d-flex flex-row justify-content-end">
-                  <span class="mr-2">
-                    <i class="fas fa-square text-primary"></i> This Week
-                  </span>
-
-                  <span>
-                    <i class="fas fa-square text-gray"></i> Last Week
-                  </span>
+                  {!! $monthlySales->container() !!}
                 </div>
               </div>
             </div>
@@ -500,4 +482,8 @@
           </div>
         <!-- /.row -->
       </div><!-- /.container-fluid -->
+@endsection
+@section('js')
+  {!! $chart->script() !!}
+  {!! $monthlySales->script() !!}
 @endsection
