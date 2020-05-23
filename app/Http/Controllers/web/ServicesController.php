0424b371d63b55b0f6ad9ca0e29ddf57397c9e8e
williamdk <asabawilliam@gmail.com>
2020-05-23 22:14:43 +0300

    add auth middleware

--- app/Http/Controllers/web/ServicesController.php
+++ app/Http/Controllers/web/ServicesController.php
@@ -10,6 +10,9 @@ use App\Model\Setting;
 use Illuminate\Http\Request;
 
 class ServicesController extends Controller {
+	public function __construct() {
+		$this->middleware('auth');
+	}
 	/**
 	 * Display a listing of the resource.
 	 *
