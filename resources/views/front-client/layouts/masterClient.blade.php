61a293fca7446c7e133ac063a3a9896b2ddeffb7
williamdk <asabawilliam@gmail.com>
2020-06-17 06:04:40 +0300

    remove new message notification

--- resources/views/front-client/layouts/masterClient.blade.php
+++ resources/views/front-client/layouts/masterClient.blade.php
@@ -58,67 +58,7 @@
 				</li>
 				@endif
 				@else
-				<li class="nav-item dropdown">
-					<a class="nav-link" data-toggle="dropdown" href="#">
-						<i class="far fa-comments pr-2 mt-2"></i>
-						<span class="badge badge-danger navbar-badge">3</span>
-					</a>
-					<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-						<a href="#" class="dropdown-item">
-							<!-- Message Start -->
-							<div class="media">
-								<img src="{{ asset('images/logo/user.png') }}" alt="User Avatar"
-									class="img-size-50 mr-3 img-circle">
-								<div class="media-body">
-									<h3 class="dropdown-item-title">
-										Brad Diesel
-										<span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-									</h3>
-									<p class="text-sm">Call me whenever you can...</p>
-									<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-								</div>
-							</div>
-							<!-- Message End -->
-						</a>
-						<div class="dropdown-divider"></div>
-						<a href="#" class="dropdown-item">
-							<!-- Message Start -->
-							<div class="media">
-								<img src="{{ asset('images/logo/user.png') }}" alt="User Avatar"
-									class="img-size-50 img-circle mr-3">
-								<div class="media-body">
-									<h3 class="dropdown-item-title">
-										John Pierce
-										<span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-									</h3>
-									<p class="text-sm">I got your message bro</p>
-									<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-								</div>
-							</div>
-							<!-- Message End -->
-						</a>
-						<div class="dropdown-divider"></div>
-						<a href="#" class="dropdown-item">
-							<!-- Message Start -->
-							<div class="media">
-								<img src="{{ asset('images/logo/user.png') }}" alt="User Avatar"
-									class="img-size-50 img-circle mr-3">
-								<div class="media-body">
-									<h3 class="dropdown-item-title">
-										Nora Silvester
-										<span class="float-right text-sm text-warning"><i
-												class="fas fa-star"></i></span>
-									</h3>
-									<p class="text-sm">The subject goes here</p>
-									<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-								</div>
-							</div>
-							<!-- Message End -->
-						</a>
-						<div class="dropdown-divider"></div>
-						<a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-					</div>
-				</li>
+
 				<!-- Notifications Dropdown Menu -->
 				<li class="nav-item dropdown">
 					<a class="nav-link" data-toggle="dropdown" href="#">
@@ -167,7 +107,7 @@
 						</div>
 							<hr>
 						<!-- my profile-->
-						<a class="dropdown-item" href="{{ route('user-profile.show', ['user' => Auth::user()]) }}">
+						<a class="dropdown-item" href="{{ route('client-profile.show', ['user' => Auth::user()]) }}">
 							<i class="far fa-user"></i>  {{ __('My Profile') }}
 						</a>
 						<!-- /.my profile-->
@@ -204,7 +144,7 @@
 			<!-- Main content -->
 			<div class="container">
 				@guest
-				{{ route('login') }}
+				<h4>You need to <a href="{{ route('login') }}">{{ __('login') }}</a> to resume your session</h4>
 				@else
 				@yield('content')
 				@endguest
