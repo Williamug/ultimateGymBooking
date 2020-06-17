046233c63338041bfcc417dbb8f2111bce2e5536
williamdk <asabawilliam@gmail.com>
2020-06-17 06:05:00 +0300

    change signature

--- resources/views/patials/footer.blade.php
+++ resources/views/patials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
 	<!-- To the right -->
 	<div class="float-right d-none d-sm-inline">
-		Powered by <a href="">AJ Systems</a>
+		Powered by <a href="">Musenyu Systems</a>
 	</div>
 	<!-- Default to the left -->
 	<strong>Copyright &copy; 2019 - {{ date('Y') }} <a href="#">{{ config('app.name') }}</a>.</strong> All rights
