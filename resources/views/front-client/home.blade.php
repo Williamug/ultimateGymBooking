f335a55f176c433bdfc4f5f18747d64dfab43f97
williamdk <asabawilliam@gmail.com>
2020-06-19 14:37:08 +0300

    update view

--- resources/views/front-client/home.blade.php
+++ resources/views/front-client/home.blade.php
@@ -64,43 +64,49 @@
 	<!-- Nutritional tip -->
 		<br><br>
 		<h4><i>Nutritional tip of the day...</i></h4>
-		<div class="card">
-        <div class="card-header">
-          <h4 class="card-title"><i>From: Janem Doe</i></h4>
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fas fa-minus"></i></button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fas fa-times"></i></button>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="row">
-          	<div class="col-10">
-          		Start creating your amazing application!
-          		Proin eget tortor risus. Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur aliquet quam id dui posuere blandit. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna dictum porta. Pellentesque in ipsum id orci porta dapibus.
-          	</div>
-          	<div class="" id="timeago">time ago</div>
-          </div>
-        </div>
-        <!-- /.card-body -->
-      </div>
+        @foreach($nutritionalTips as $tip)
+            <div class="card card-widget">
+              <div class="card-header">
+                <div class="user-block">
+                    @if($tip->user->instructor->profile_image != null)
+                        <img src="{{ asset('storage/' . $tip->user->instructor->profile_image) }}" class="img-circle elevation-1" alt="User Image" width="35" height="35">
+                    @else
+                        <img src="{{ asset('images/profiles/profile.png') }}" class="img-circle elevation-1" alt="User Image" width="35" height="35">
+                            {{-- {{ Auth::user()->name }} <span class="caret"></span> --}}
+                    @endif
+                  <span class="username"><a href="#">{{ $tip->user->name }}</a></span>
+                  <span class="description">Shared publicly - {{ $tip->created_at->diffForHumans() }}</span>
+                </div>
+                <!-- /.user-block -->
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+                <!-- /.card-tools -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body" style="display: block;">
+                <!-- post text -->
+                <p>
+                  {{ Str::limit(strip_tags($tip->post), 50) }}
+                  @if(strlen(strip_tags($tip->post)) > 50)<a href="{{ route('nutrition-tip.show', ['tip' => $tip]) }}">Read More</a>
+                  @endif
+                </p>
 
-      <!-- comments -->
-    <div class="card">
-         <!-- form start -->
-         <form class="form-horizontal">
-            <div class="card-body">
-               	<div class="form-group">
-                    <label>Comment</label>
-                    <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                <!-- Attachment -->
+                <div>
+
                 </div>
-                <div class="form-group">
-                <button type="button" class="btn bg-gradient-primary btn-sm">Submit</button>
-                </div>
+                <!-- /.attachment-block -->
+
+                <!-- Social sharing buttons -->
+                {{-- <button type="button" class="btn btn-default btn-sm"><i class="far fa-thumbs-up"></i> Like</button> --}}
+                {{-- <span class="float-right text-muted">45 likes - 2 comments</span> --}}
+              </div>
+              <!-- /.card-body -->
+
             </div>
-        </form>
-    </div>
+        @endforeach
 </div><!-- /.container-fluid -->
 @endsection
